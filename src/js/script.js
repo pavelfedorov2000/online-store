9042f4917e996fbb7adc8c5ea05b4f5c5cd9c69f
Pavel <pavelfedorov929@gmail.com>
2021-08-10 08:09:03 +0300

    заготовка для интернет-магазина

---
+++ src/js/script.js
@@ -0,0 +1,30 @@
+$(function () {
+
+    @@include('../blocks/modules/burger/burger.js')
+
+    @@include('../blocks/modules/aside-filters/aside-filters.js')
+
+    @@include('../blocks/modules/popups/popups.js')
+
+    @@include('tabs.js')
+
+    @@include('accordion.js')
+
+    @@include('sliders.js')
+
+    @@include('rate.js')
+
+    @@include('range-slider.js')
+
+    @@include('validate.js')
+
+    @@include('scroll.js')
+
+    @@include('dinamic-adapt.js')
+
+    $('.filter-style').styler();
+
+});
+
+
+
